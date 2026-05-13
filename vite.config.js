8edf7a7a384180d2da66679184a5e98071b4cb82
aliciarogers01 <aleigh0435@gmail.com>
2026-05-12 20:59:20 -0400

    Fix GitHub Pages base paths

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/graverobberband/",
   plugins: [react()],
   build: {
     outDir: "docs",
